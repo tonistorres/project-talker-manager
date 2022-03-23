9cc038ea753434eab070f1c0f2a618c44b94b9af
tonistorres <sisvenda2011@gmail.com>
2022-03-23 12:08:04 -0300

    refactor: Limpando validationEmail

--- util/validationEmail.js
+++ util/validationEmail.js
@@ -1,6 +1,3 @@
-/* A função valida email, se utiliza de um regex para fazer tal validação 
-o retorno dessa função é do tipo boolean true e false 
-*/
 function validEmail(email) {
     return /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/.test(email);
 }
